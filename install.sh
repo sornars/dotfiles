5ef5385c19be7ca1750d6b3a651b10246677615b
Shivan Sornarajah <github@sornars.com>
2015-06-06 15:38:07 +0100

    Added .gitconfig and .global-gitignore. Updated install.sh.

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 dir=~/dotfiles
-files=".bashrc .vimrc .vim .git-prompt.sh"
+files=".bashrc .vimrc .vim .git-prompt.sh .gitconfig .global-gitignore"
 
 mkdir -p $dir/backup
 
 for file in $files; do
-    mv ~/$file $dir/backup/$file.bak
-    ln -s $dir/$file ~/$file
+    if [ ! -h ~/$file ]; then
+        echo "Creating symlink for ~/$file"
+        mv ~/$file $dir/backup/$file.bak
+        ln -s $dir/$file ~/$file
+    fi
 done
