5e655bba1c12e073c352cdd6dfa6ba181c989aa0
Shivan Sornarajah <sornars@sornars.com>
2015-09-21 20:23:48 +0100

    Added .xinitrc and .bash_profile.

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 dir=~/dotfiles
-files=".bashrc .vimrc .vim .git-prompt.sh .gitconfig .global-gitignore .tmux.conf .Xresources"
+files=".bashrc .vimrc .vim .git-prompt.sh .gitconfig .global-gitignore .tmux.conf .Xresources .xinitrc .bash_profile"
 
 mkdir -p $dir/backup
 
